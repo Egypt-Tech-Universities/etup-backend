444c935dbc1a35c4e09e86519f7894a5ff88645f
moamenawadx <moamenqasas@gmail.com>
2026-07-04 18:19:11 +0300

    Fix Port problem

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,7 @@ async function bootstrap() {
 
   // 6) Start server
   const port = process.env.PORT || 8000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 
   console.log(`🚀 Server running on: http://localhost:${port}`);
   console.log(`📚 Swagger docs: http://localhost:${port}/api/docs`);
